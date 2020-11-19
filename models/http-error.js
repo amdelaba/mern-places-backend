a3343e2741aa1e7f9d508766a1714b6ed2f2290a
Andres De La Barra <adelabarra24@gmail.com>
2020-11-18 19:37:39 -0500

    Added Error model

---
+++ models/http-error.js
@@ -0,0 +1,8 @@
+class HttpError extends Error {
+  constructor(message, errorCode) {
+    super(message); // adds message property 
+    this.code = errorCode;
+  }
+}
+
+module.exports = HttpError;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error')
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -22,10 +24,8 @@ router.get('/:pid', (req, res, next) => {
   const place = DUMMY_PLACES.find(place => place.id === placeId);
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided Id')
-    error.code = 404;
     // Can use either throw or next but must use next() if working with synchrnous functions (ie DB)
-    throw error;
+    throw new HttpError('Could not find a place for the provided Id', 404)
   }
   res.json({ place });
 });
@@ -35,10 +35,8 @@ router.get('/user/:uid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => p.creator === userId);
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided UserId')
-    error.code = 404;
     // Can use either throw or next but must use next() if working with synchrnous functions (ie DB)
-    return next(error);
+    return next(new HttpError('Could not find a place for the provided UserId', 404));
   }
 
   res.json({ place });

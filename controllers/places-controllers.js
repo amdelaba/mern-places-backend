d323a94ee08b2c15a74fd4aecd710ea0d070c19e
Andres De La Barra <adelabarra24@gmail.com>
2020-11-19 15:54:00 -0500

    Added input validation to post requests using express-validator package

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require('express-validator')
  
 const HttpError = require('../models/http-error');
 
@@ -38,6 +39,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace =  (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()){
+    console.log(errors);
+    throw new HttpError('Invalid inputs', 422)
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuidv4(),
@@ -52,6 +60,13 @@ const createPlace =  (req, res, next) => {
 };
 
 const updatePlace =  (req, res, next) => {
+  
+  const errors = validationResult(req);
+  if (!errors.isEmpty()){
+    console.log(errors);
+    throw new HttpError('Invalid inputs', 422)
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
   const updatePlace = { ...DUMMY_PLACES.find(place => place.id === placeId) };
@@ -70,7 +85,7 @@ const updatePlace =  (req, res, next) => {
 
 const deletePlace =  (req, res, next) => {
   const placeId = req.params.pid;
-  if (!placeId) {
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
     throw new HttpError('Could not find a place for the provided Id', 404)
   }
   DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId)
